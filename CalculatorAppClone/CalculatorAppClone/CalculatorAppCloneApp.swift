03b5f0720eedf05ad1cf1dac50f7aa1078d91b73
Jijoo Maeng <mjiju06@gmail.com>
2025-06-03 23:45:56 +0900

    feat: first commit

---
+++ CalculatorAppClone/CalculatorAppClone/CalculatorAppCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorAppCloneApp.swift
+//  CalculatorAppClone
+//
+//  Created by JiJooMaeng on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorAppCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
